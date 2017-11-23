dd9def6f9c60241c3478af6cdf6b4e6dbaf8d877
Muhammad Usman <ualeem@karwasolutions.com>
2017-11-23 09:28:11 +0300

    Base Class for side drawer

--- KarwaRide/KarwaRide/KarwaRide/ViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: KSBaseDrawerRootViewController {
 
     
-    @IBOutlet weak var Open: UIBarButtonItem!
+    //@IBOutlet weak var Open: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //KSWebClient.sharedInstance.testServerCall()
-        Open.target = self.revealViewController()
-        Open.action = #selector(SWRevealViewController.revealToggle(_:))
+        ///Open.target = self.revealViewController()
+        //Open.action = #selector(SWRevealViewController.revealToggle(_:))
     }
 
     override func didReceiveMemoryWarning() {
