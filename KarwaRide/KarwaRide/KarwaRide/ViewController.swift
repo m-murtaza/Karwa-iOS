9cf211035a087fa20825d2d1c6833a4c4de35b68
Muhammad Usman <ualeem@karwasolutions.com>
2017-11-23 08:56:58 +0300

    Basic Sidedrawer implemented.

--- KarwaRide/KarwaRide/KarwaRide/ViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/ViewController.swift
@@ -12,12 +12,14 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var Open: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //KSWebClient.sharedInstance.testServerCall()
-    
+        Open.target = self.revealViewController()
+        Open.action = #selector(SWRevealViewController.revealToggle(_:))
     }
 
     override func didReceiveMemoryWarning() {
