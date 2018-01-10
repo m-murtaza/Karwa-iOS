b2a9b8fe92f164490ebb6a391b51f70cc1451614
Muhammad Usman <ualeem@karwasolutions.com>
2018-01-10 12:43:43 +0300

    UI integration main menu

--- KarwaRide/KarwaRide/KarwaRide/Classes/Views/Controllers/Main/KTMainViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/Classes/Views/Controllers/Main/KTMainViewController.swift
@@ -25,6 +25,16 @@ class KTMainViewController: KTBaseViewController {
         }
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
