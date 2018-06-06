20431bb8f97143effae279ac1f90aa667a27f6c3
Muhammad Usman <ualeem@karwasolutions.com>
2018-06-06 10:33:34 +0300

    Add Fare Detail web View

---
+++ KarwaRide/KarwaRide/KarwaRide/Classes/Views/Controllers/Fare/KTFareHTMLViewController.swift
@@ -0,0 +1,44 @@
+//
+//  KTFareHTMLViewController.swift
+//  KarwaRide
+//
+//  Created by Muhammad Usman on 6/6/18.
+//  Copyright © 2018 Karwa. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class KTFareHTMLViewController: KTBaseDrawerRootViewController,WKNavigationDelegate {
+
+    @IBOutlet weak var webView : WKWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        webView?.navigationDelegate = self //as! WKNavigationDelegate
+        let request = URLRequest(url: URL(string: "http://www.karwatechnologies.com/fare.htm")!)
+        webView?.load(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finished navigating to url \(String(describing: webView.url))")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
