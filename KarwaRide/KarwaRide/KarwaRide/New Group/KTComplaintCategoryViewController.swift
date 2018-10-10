c8db5bd95e977917290c78ceb559ad8fc02c6937
Sam <osama.ashraf2005@gmail.com>
2018-10-10 11:49:03 +0300

    Finalizing Complaints category selection UI

--- KarwaRide/KarwaRide/KarwaRide/New Group/KTComplaintCategoryViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/New Group/KTComplaintCategoryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Spring
 
 class KTComplaintCategoryViewController: KTBaseDrawerRootViewController,KTComplaintCategoryViewModelDelegate,UITableViewDelegate,UITableViewDataSource
 {
@@ -14,6 +15,12 @@ class KTComplaintCategoryViewController: KTBaseDrawerRootViewController,KTCompla
     @IBOutlet weak var tblView: UITableView!
     private var vModel : KTComplaintCategoryViewModel?
 
+    @IBOutlet weak var btnComplaints: SpringButton!
+    @IBOutlet weak var complaintSelector: UIImageView!
+    
+    @IBOutlet weak var btnLostItems: SpringButton!
+    @IBOutlet weak var lostItemsSelector: UIImageView!
+    
     override func viewDidLoad()
     {
         self.viewModel = KTComplaintCategoryViewModel(del: self)
@@ -28,6 +35,20 @@ class KTComplaintCategoryViewController: KTBaseDrawerRootViewController,KTCompla
         return (vModel?.numberOfRows())!
     }
     
+    @IBAction func btnBackTapped(_ sender: Any)
+    {
+        self.dismiss()
+    }
+
+    @IBAction func complaintTapped(_ sender: Any)
+    {
+        vModel?.complaintTapped()
+    }
+    @IBAction func lostItemsTapped(_ sender: Any)
+    {
+        vModel?.lostItemTapped()
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell : KTComplaintsCategoryTableViewCell = tableView.dequeueReusableCell(withIdentifier: "ComplaintCategoryTableViewCellIdentifier") as! KTComplaintsCategoryTableViewCell
@@ -58,6 +79,14 @@ class KTComplaintCategoryViewController: KTBaseDrawerRootViewController,KTCompla
         tblView.reloadData()
     }
 
+    func toggleTab(showSecondTab isComplaintsVisible : Bool)
+    {
+        btnComplaints.setTitleColor(isComplaintsVisible ? UIColor.gray : UIColor.init(hex: "#006170"), for: .normal)
+        btnLostItems.setTitleColor(isComplaintsVisible ? UIColor.init(hex: "#006170") : UIColor.gray, for: .normal)
+        lostItemsSelector.isHidden = !isComplaintsVisible
+        complaintSelector.isHidden = isComplaintsVisible
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         //        if segue.identifier == "segueNotificationToDetail" {
--- KarwaRide/KarwaRide/KarwaRide/New Group1/KTComplaintCategoryViewModel.swift
+++ KarwaRide/KarwaRide/KarwaRide/New Group1/KTComplaintCategoryViewModel.swift
@@ -12,15 +12,16 @@ protocol KTComplaintCategoryViewModelDelegate: KTViewModelDelegate
 {
     func reloadTableData()
     func showDetail()
+    func toggleTab(showSecondTab isComplaintsVisible : Bool)
 }
 
 class KTComplaintCategoryViewModel: KTBaseViewModel
 {
-    var complaintsCategories : [ComplaintCategoryModel] = []
-    var lostItemsCategories : [ComplaintCategoryModel] = []
+    var categories : [ComplaintCategoryModel] = []
     var del : KTComplaintCategoryViewModelDelegate?
     var selectedBooking :KTBooking?
-    
+    var isComplaintsShowing = true
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -30,26 +31,46 @@ class KTComplaintCategoryViewModel: KTBaseViewModel
     
     func numberOfRows() -> Int
     {
-        print("Count is: \(complaintsCategories.count)")
-        return complaintsCategories.count
+        return categories.count
     }
     
     func categoryName(forCellIdx idx: Int) -> String
     {
-        print(String("Name: " + complaintsCategories[idx].title))
-        return complaintsCategories[idx].title
+        return categories[idx].title
     }
     
     func description(forCellIdx idx: Int) -> String
     {
-        print(String("Desc: " + complaintsCategories[idx].desc))
-        return complaintsCategories[idx].desc
+        return categories[idx].desc
     }
     
     func notificationIcon(forCellIdx idx: Int) -> UIImage
     {
-        print(String("Image: " + complaintsCategories[idx].image))
-        return UIImage(named: complaintsCategories[idx].image)!
+        return UIImage(named: categories[idx].image)!
+    }
+
+    func complaintTapped()
+    {
+        if(!isComplaintsShowing)
+        {
+            categories.removeAll()
+            getComplaintsCategories()
+            self.del?.reloadTableData()
+            self.del?.toggleTab(showSecondTab: isComplaintsShowing)
+            isComplaintsShowing = !isComplaintsShowing
+        }
+    }
+    
+    func lostItemTapped()
+    {
+        if(isComplaintsShowing)
+        {
+            categories.removeAll()
+            getLostAndFoundCategories()
+            self.del?.reloadTableData()
+            self.del?.toggleTab(showSecondTab: isComplaintsShowing)
+            isComplaintsShowing = !isComplaintsShowing
+        }
     }
     
     func fetchnComplaintsCategories()
@@ -60,25 +81,23 @@ class KTComplaintCategoryViewModel: KTBaseViewModel
     
     func getComplaintsCategories()
     {
-        complaintsCategories.append(ComplaintCategoryModel(12, "ico_vehicle", "VEHICLE ISSUES", "Cleanliness, A/C, Printer not working, etc"))
-        complaintsCategories.append(ComplaintCategoryModel(14, "ico_driver", "DRIVER ISSUES", "Mis-behaved, Speaking negatively, Smoking, etc"))
-        complaintsCategories.append(ComplaintCategoryModel(15, "ico_fare", "FARE ISSUES", "Tampering, Long route, Meter not working, etc"))
-        complaintsCategories.append(ComplaintCategoryModel(13, "ico_safety", "SAFETY ISSUES", "Lane discipline, Over speeding, Poor driving, etc"))
-        complaintsCategories.append(ComplaintCategoryModel(-1, "ico_other", "I HAVE DIFFERENT ISSUES", "Some feedback, quality improvements, etc"))
+        categories.append(ComplaintCategoryModel(12, "ico_vehicle", "VEHICLE ISSUES", "Cleanliness, A/C, Printer not working, etc"))
+        categories.append(ComplaintCategoryModel(14, "ico_driver", "DRIVER ISSUES", "Mis-behaved, Speaking negatively, Smoking, etc"))
+        categories.append(ComplaintCategoryModel(15, "ico_fare", "FARE ISSUES", "Tampering, Long route, Meter not working, etc"))
+        categories.append(ComplaintCategoryModel(13, "ico_safety", "SAFETY ISSUES", "Lane discipline, Over speeding, Poor driving, etc"))
+        categories.append(ComplaintCategoryModel(-1, "ico_other", "I HAVE DIFFERENT ISSUES", "Some feedback, quality improvements, etc"))
     }
     
-    func getLostAndFoundCategories() -> [ComplaintCategoryModel]
+    func getLostAndFoundCategories()
     {
-        lostItemsCategories.append(ComplaintCategoryModel(1, "ico_personal_items", "PERSONAL ITEMS", "Handbag, Keys, Luggage, Clothes, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(2, "ico_appliances", "APPLIANCES", "LED TV, Heater, Microwave, Speakers, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(3, "ico_electronics", "ELECTRONICS", "Mobile, Camera, Tablet, Laptop, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(4, "ico_documents", "DOCUMENTS", "ertificates, Books, Passport, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(10, "ico_valuables", "VALUABLES", "Gold, Cash, Silver Jewelry, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(18, "ico_cards", "CARDS", "QID, Driving license, ATM, Credit Card, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(20, "ico_sports", "SPORTS ITEMS", "Football, Cricket, Tennis, Badminton, etc"))
-        lostItemsCategories.append(ComplaintCategoryModel(-1, "ico_others", "OTHERS", "I have lost something else"))
-        
-        return lostItemsCategories;
+        categories.append(ComplaintCategoryModel(1, "ico_personal_items", "PERSONAL ITEMS", "Handbag, Keys, Luggage, Clothes, etc"))
+        categories.append(ComplaintCategoryModel(2, "ico_appliances", "APPLIANCES", "LED TV, Heater, Microwave, Speakers, etc"))
+        categories.append(ComplaintCategoryModel(3, "ico_electronics", "ELECTRONICS", "Mobile, Camera, Tablet, Laptop, etc"))
+        categories.append(ComplaintCategoryModel(4, "ico_documents", "DOCUMENTS", "ertificates, Books, Passport, etc"))
+        categories.append(ComplaintCategoryModel(10, "ico_valuables", "VALUABLES", "Gold, Cash, Silver Jewelry, etc"))
+        categories.append(ComplaintCategoryModel(18, "ico_cards", "CARDS", "QID, Driving license, ATM, Credit Card, etc"))
+        categories.append(ComplaintCategoryModel(20, "ico_sports", "SPORTS ITEMS", "Football, Cricket, Tennis, Badminton, etc"))
+        categories.append(ComplaintCategoryModel(-1, "ico_others", "OTHERS", "I have lost something else"))
     }
 
     func rowSelected(atIndex idx: Int)
