339944b934ba79f4b47a394f3da520083ba11e7d
Sam <osama.ashraf2005@gmail.com>
2019-05-17 23:37:40 +0300

    hiding S N P

--- KarwaRide/KarwaRide/KarwaRide/Classes/Views/Controllers/SideDrawer/KTLeftMenuViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/Classes/Views/Controllers/SideDrawer/KTLeftMenuViewController.swift
@@ -94,55 +94,18 @@ class KTLeftMenuViewController: KTBaseViewController, UITableViewDelegate,UITabl
 //        }
 
         /* With scan N pay */
-        switch indexPath.row {
-        case 0:
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "BookingNavigationViewController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        case 1:
-
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "MyTirpsNavigationController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        case 2:
-
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "NotificationNavigationController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        case 3:
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "KTFareNavigation")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        case 4:
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "PaymentNavigationController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        case 5:
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "SettingsNavigationController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-
-        default:
-            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "UnderConstructionNavigationController")
-            sideMenuViewController?.hideMenuViewController()
-            break
-        }
-        
-        
-        
-        /* Without scan N pay */
 //        switch indexPath.row {
 //        case 0:
 //            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "BookingNavigationViewController")
 //            sideMenuViewController?.hideMenuViewController()
 //            break
 //        case 1:
-//            
+//
 //            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "MyTirpsNavigationController")
 //            sideMenuViewController?.hideMenuViewController()
 //            break
 //        case 2:
-//            
+//
 //            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "NotificationNavigationController")
 //            sideMenuViewController?.hideMenuViewController()
 //            break
@@ -151,15 +114,52 @@ class KTLeftMenuViewController: KTBaseViewController, UITableViewDelegate,UITabl
 //            sideMenuViewController?.hideMenuViewController()
 //            break
 //        case 4:
+//            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "PaymentNavigationController")
+//            sideMenuViewController?.hideMenuViewController()
+//            break
+//        case 5:
 //            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "SettingsNavigationController")
 //            sideMenuViewController?.hideMenuViewController()
 //            break
-//            
+//
 //        default:
 //            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "UnderConstructionNavigationController")
 //            sideMenuViewController?.hideMenuViewController()
 //            break
 //        }
+        
+        
+        
+        /* Without scan N pay */
+        switch indexPath.row {
+        case 0:
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "BookingNavigationViewController")
+            sideMenuViewController?.hideMenuViewController()
+            break
+        case 1:
+            
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "MyTirpsNavigationController")
+            sideMenuViewController?.hideMenuViewController()
+            break
+        case 2:
+            
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "NotificationNavigationController")
+            sideMenuViewController?.hideMenuViewController()
+            break
+        case 3:
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "KTFareNavigation")
+            sideMenuViewController?.hideMenuViewController()
+            break
+        case 4:
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "SettingsNavigationController")
+            sideMenuViewController?.hideMenuViewController()
+            break
+            
+        default:
+            sideMenuViewController?.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "UnderConstructionNavigationController")
+            sideMenuViewController?.hideMenuViewController()
+            break
+        }
         return false
     }
 }
