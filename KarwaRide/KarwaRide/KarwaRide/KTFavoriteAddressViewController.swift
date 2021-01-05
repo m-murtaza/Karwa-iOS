481dea87b0710df5244c2ae5e39dacdf165078a5
Sam <osama.ashraf2005@gmail.com>
2021-01-05 11:15:03 +0300

    Adding Favorites to Arabic

--- KarwaRide/KarwaRide/KarwaRide/KTFavoriteAddressViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/KTFavoriteAddressViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class KTFavoriteAddressViewController: KTBaseViewController {
   
   @IBOutlet weak var navigationBar: UIView!
-  @IBOutlet weak var locationNameTextField: KTTextField!
-  @IBOutlet weak var locationTextField: KTTextField!
+  @IBOutlet weak var locationNameTextField: MDCFilledTextField!
+  @IBOutlet weak var locationTextField: MDCFilledTextField!
   
   var favoritelocation: KTGeoLocation?
   
@@ -27,14 +28,35 @@ class KTFavoriteAddressViewController: KTBaseViewController {
   private func setupUI() {
     changeStatusBarColor(color: UIColor.primaryLight)
     navigationBar.backgroundColor = UIColor.primaryLight
-    locationNameTextField.placeHolder = "txt_loc_name".localized()
-    locationTextField.placeHolder = "txt_location_head".localized()
+
+    locationNameTextField.label.text = "txt_loc_name".localized()
+    locationNameTextField.label.font = UIFont(name: "MuseoSans-500", size: 11.0)!
+    locationNameTextField.setUnderlineColor(UIColor(hexString: "#005866"), for: .editing)
+    locationNameTextField.setUnderlineColor(UIColor(hexString: "#005866"), for: .normal)
+    locationNameTextField.label.textColor = UIColor(hexString: "#6CB1B7")
+    locationNameTextField.tintColor = UIColor(hexString: "#6CB1B7")
+    locationNameTextField.inputView?.tintColor = UIColor(hexString: "#6CB1B7")
+    locationNameTextField.label.textColor = UIColor(hexString: "#6CB1B7")
+    locationNameTextField.setFilledBackgroundColor(UIColor(hexString: "#FFFFFF"), for: .normal)
+    locationNameTextField.setFilledBackgroundColor(UIColor(hexString: "#FFFFFF"), for: .editing)
     
-    locationTextField.textField.delegate = self
-    locationNameTextField.textField.delegate = self
+    locationTextField.label.text = "txt_loc_name".localized()
+    locationTextField.label.font = UIFont(name: "MuseoSans-500", size: 11.0)!
+    locationTextField.setUnderlineColor(UIColor(hexString: "#005866"), for: .editing)
+    locationTextField.setUnderlineColor(UIColor(hexString: "#005866"), for: .normal)
+    locationTextField.label.textColor = UIColor(hexString: "#6CB1B7")
+    locationTextField.tintColor = UIColor(hexString: "#6CB1B7")
+    locationTextField.inputView?.tintColor = UIColor(hexString: "#6CB1B7")
+    locationTextField.label.textColor = UIColor(hexString: "#6CB1B7")
+    locationTextField.setFilledBackgroundColor(UIColor(hexString: "#FFFFFF"), for: .normal)
+
+//    locationTextField.label.text = "txt_location_head".localized()
+    
+//    locationTextField.textField.delegate = self
+//    locationNameTextField.textField.delegate = self
     
     if let loc = favoritelocation {
-      locationTextField.textField.text = loc.name
+      locationTextField.label.text = loc.name
     }
     if let name = favoritelocation?.favoriteName {
       locationNameTextField.text = name
@@ -42,10 +64,11 @@ class KTFavoriteAddressViewController: KTBaseViewController {
     locationTextField.isUserInteractionEnabled = false
     
     DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-      self.locationNameTextField.textField.becomeFirstResponder()
+        self.locationNameTextField.becomeFirstResponder()
     }
-    locationTextField.textField.titleColor = #colorLiteral(red: 0, green: 0.3803921569, blue: 0.4392156863, alpha: 0.5)
-    locationTextField.textField.textColor = #colorLiteral(red: 0, green: 0.3803921569, blue: 0.4392156863, alpha: 0.5)
+
+//    locationTextField.textField.titleColor = #colorLiteral(red: 0, green: 0.3803921569, blue: 0.4392156863, alpha: 0.5)
+//    locationTextField.textField.textColor = #colorLiteral(red: 0, green: 0.3803921569, blue: 0.4392156863, alpha: 0.5)
   }
   
   @IBAction func saveLocationAction(_ sender: UIButton) {
@@ -73,24 +96,24 @@ extension KTFavoriteAddressViewController: KTAddressFavoriteViewModelDelegate {
   
 }
 
-extension KTFavoriteAddressViewController: UITextFieldDelegate {
-  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-    if textField == locationTextField.textField {
-      locationTextField.textFieldState = .focused
-    }
-    if textField == locationNameTextField.textField {
-      locationNameTextField.textFieldState = .focused
-    }
-    return true
-  }
-  
-  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-    if textField == locationTextField.textField {
-      locationTextField.textFieldState = .normal
-    }
-    if textField == locationNameTextField.textField {
-      locationNameTextField.textFieldState = .normal
-    }
-    return true
-  }
-}
+//extension KTFavoriteAddressViewController: UITextFieldDelegate {
+//  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//    if textField == locationTextField.textField {
+//      locationTextField.textFieldState = .focused
+//    }
+//    if textField == locationNameTextField.textField {
+//      locationNameTextField.textFieldState = .focused
+//    }
+//    return true
+//  }
+//
+//  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//    if textField == locationTextField.textField {
+//      locationTextField.textFieldState = .normal
+//    }
+//    if textField == locationNameTextField.textField {
+//      locationNameTextField.textFieldState = .normal
+//    }
+//    return true
+//  }
+//}
