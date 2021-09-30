b6c393b324d34fd3140a7a15383299be388e4717
satheesh kumar <satheesh.k@molekule.com>
2021-09-30 16:31:35 +0530

    session pop up issue fixed

--- KarwaRide/KarwaRide/KarwaRide/Classes/DAL/KTUserManager+Logout.swift
+++ KarwaRide/KarwaRide/KarwaRide/Classes/DAL/KTUserManager+Logout.swift
@@ -7,57 +7,65 @@
 //
 
 import Foundation
-extension KTUserManager
-{
-  
-  func logout() {
+
+var sessionExpiredPopUpShown = false
+
+extension KTUserManager {
     
-    let alertController = UIAlertController(title: "Session Expired", message: "Please login again", preferredStyle: .alert)
-    
-    //let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
-      
-      self.logout { (status, response) in
-        print("Logout on server " + status)
-        self.removeUserData()
-        self.removeNotification()
-        KTPaymentManager().removeAllPaymentData()
-        PreviousSelectedPayment.shared.selectedPaymentMethod = nil
-        PreviousSelectedPayment.shared.rebook = false
-        (UIApplication.shared.delegate as! AppDelegate).showLogin()
+    func logout() {
         
-      }
+        if sessionExpiredPopUpShown == false {
+            
+            let alertController = UIAlertController(title: "str_session_expired".localized(), message: "str_login_again".localized(), preferredStyle: .alert)
+            
+            //let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+                
+                self.logout { (status, response) in
+                    print("Logout on server " + status)
+                    self.removeUserData()
+                    self.removeNotification()
+                    KTPaymentManager().removeAllPaymentData()
+                    PreviousSelectedPayment.shared.selectedPaymentMethod = nil
+                    PreviousSelectedPayment.shared.rebook = false
+                    sessionExpiredPopUpShown = false
+                    (UIApplication.shared.delegate as! AppDelegate).showLogin()
+                }
+            }
+            alertController.addAction(okAction)
+            
+            let appDelegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.showAlter(alertController: alertController)
+            sessionExpiredPopUpShown = true
+        }
+        
+        
     }
-    alertController.addAction(okAction)
     
-    let appDelegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
-    appDelegate.showAlter(alertController: alertController)
-  }
-  
-  func logout(completion completionBlock:@escaping KTDALCompletionBlock)
-  {
-    
-    //Do not call self.post as we don't need to handle error and self.post will handle error
-    KTWebClient.sharedInstance.get(uri: Constants.APIURL.Logout, param: nil) { (status, response) in
-      
-      completionBlock(Constants.APIResponseStatus.SUCCESS, response)  //No need to send
+    func logout(completion completionBlock:@escaping KTDALCompletionBlock)
+    {
+        
+        //Do not call self.post as we don't need to handle error and self.post will handle error
+        KTWebClient.sharedInstance.get(uri: Constants.APIURL.Logout, param: nil) { (status, response) in
+            
+            completionBlock(Constants.APIResponseStatus.SUCCESS, response)  //No need to send
+        }
     }
-  }
-  
-  func removeNotification() {
-    KTNotificationManager().deleteAllNotifications()
-  }
-  
-  func removeUserData()  {
-    KTDALManager().removeSyncTime(forKey: BOOKING_SYNC_TIME)
-    KTDALManager().removeSyncTime(forKey: USER_PREF_SYNC_TIME)
-    KTAppSessionInfo.currentSession.removeCurrentSession()
-    KTBookmark.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    KTFavorites.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    KTUser.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    KTBooking.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    KTNotification.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    KTTransactions.mr_truncateAll(in: NSManagedObjectContext.mr_default())
-    NSManagedObjectContext.mr_default().mr_saveToPersistentStoreAndWait()
-  }
+    
+    func removeNotification() {
+        KTNotificationManager().deleteAllNotifications()
+    }
+    
+    func removeUserData()  {
+        KTDALManager().removeSyncTime(forKey: BOOKING_SYNC_TIME)
+        KTDALManager().removeSyncTime(forKey: USER_PREF_SYNC_TIME)
+        KTAppSessionInfo.currentSession.removeCurrentSession()
+        KTBookmark.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        KTFavorites.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        KTUser.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        KTBooking.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        KTNotification.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        KTTransactions.mr_truncateAll(in: NSManagedObjectContext.mr_default())
+        NSManagedObjectContext.mr_default().mr_saveToPersistentStoreAndWait()
+    }
 }
