bcae462d893a73f13c036603a53fea0f67b9448d
Mashood Murtaza <mashood.murtaza@piecyfer.com>
2021-10-20 19:21:02 +0500

    worked on carousel view

--- KarwaRide/KarwaRide/KarwaRide/VehicleDetailBottomSheetVC.swift
+++ KarwaRide/KarwaRide/KarwaRide/VehicleDetailBottomSheetVC.swift
@@ -13,6 +13,7 @@ import ABLoaderView
 import FittedSheets
 import UIKit
 import Spring
+import UPCarouselFlowLayout
 
 class VehicleDetailBottomSheetVC: KTBaseViewController, Draggable {
     
@@ -30,6 +31,7 @@ class VehicleDetailBottomSheetVC: KTBaseViewController, Draggable {
     @IBOutlet weak var lblPromo: SpringLabel!
     @IBOutlet weak var lblTotal: SpringLabel!
     @IBOutlet weak var btnRequestBooking: SpringButton!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     @IBOutlet weak var heightOFScrollViewContent: NSLayoutConstraint!
     
@@ -37,11 +39,14 @@ class VehicleDetailBottomSheetVC: KTBaseViewController, Draggable {
     var sheet: SheetViewController?
     var sheetCoordinator: UBottomSheetCoordinator?
     var oneTimeSetSizeForBottomSheet = false
+    var screenSize: CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenSize = UIScreen.main.bounds
         self.sheet?.handleScrollView(self.scrollView)
         self.sheet?.view.backgroundColor = .clear
+        self.setupCV()
     }
     
     override func viewWillAppear(_ animated: Bool){
@@ -68,6 +73,7 @@ class VehicleDetailBottomSheetVC: KTBaseViewController, Draggable {
         }
         
 //        self.sheet?.handleScrollView(self.scrollView)
+        collectionView.reloadData()
     }
     
     @IBAction func btnRequestBookingTouchDown(_ sender: SpringButton){
@@ -81,5 +87,56 @@ class VehicleDetailBottomSheetVC: KTBaseViewController, Draggable {
     @IBAction func btnRequestBooking(_ sender: Any){
       springAnimateButtonTapOut(button: btnRequestBooking)
 //      (viewModel as! KTCreateBookingViewModel).btnRequestBookingTapped()
+    }
+}
+
+extension VehicleDetailBottomSheetVC: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: VehicleDetailCarouselCell.self), for: indexPath) as! VehicleDetailCarouselCell
+
+        return cell
+    }
+}
+
+extension VehicleDetailBottomSheetVC: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        (viewModel as! KTCreateBookingViewModel).vehicleTypeTapped(idx: indexPath.row)
+        //self.veiwFareBreakdown.isHidden = false
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width * 0.8, height: collectionView.frame.height)
+    }
+    
+    func setupCV(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: String(describing: VehicleDetailCarouselCell.self), bundle:nil), forCellWithReuseIdentifier: String(describing: VehicleDetailCarouselCell.self))
+        let layout = UPCarouselFlowLayout()
+        layout.spacingMode = UPCarouselFlowLayoutSpacingMode.fixed(spacing: 10)
+        layout.sideItemScale = 0.8
+        layout.itemSize = CGSize(width: collectionView.frame.width * 0.8,
+        height: collectionView.frame.height)
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+    }
+}
+
+extension VehicleDetailBottomSheetVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
     }
 }
---
+++ KarwaRide/KarwaRide/KarwaRide/VehicleDetailCarouselCell.swift
@@ -0,0 +1,19 @@
+//
+//  VehicleDetailCarouselCell.swift
+//  KarwaRide
+//
+//  Created by Piecyfer on 20/10/2021.
+//  Copyright © 2021 Karwa. All rights reserved.
+//
+
+import Foundation
+import ScalingCarousel
+
+class VehicleDetailCarouselCell: UICollectionViewCell {
+    @IBOutlet weak var imgBg : UIImageView!
+    @IBOutlet weak var imgVehicleType : UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
