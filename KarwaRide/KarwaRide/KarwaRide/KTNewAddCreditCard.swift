78b132346b3f02744eefdc1891f71ddaed78ce6e
osamaashraf2005 <osama.ashraf2005@gmail.com>
2021-12-13 14:39:57 +0300

    Adding new VC for Adding Card

--- KarwaRide/KarwaRide/KarwaRide/KTNewAddCreditCard.swift
+++ KarwaRide/KarwaRide/KarwaRide/KTNewAddCreditCard.swift
@@ -7,7 +7,72 @@
 //
 
 import Foundation
+import WebKit
 
-class KTNewAddCreditCard: KTBaseDrawerRootViewController {
+class KTNewAddCreditCard: KTBaseDrawerRootViewController, KTNewAddCreditCardVMDelegate, WKNavigationDelegate, WKUIDelegate,  WKScriptMessageHandler {
+
+    @IBOutlet weak var ktWebView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ktWebView.navigationDelegate = self
+        ktWebView.uiDelegate = self
+        ktWebView.configuration.userContentController.add(self, name: "karwa_hook")
+    }
     
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        //This function handles the events coming from javascript. We'll configure the javascript side of this later.
+        //We can access properties through the message body, like this:
+        guard let response = message.body as? String else { return }
+        print(response)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        //This function is called when the webview finishes navigating to the webpage.
+        //We use this to send data to the webview when it's loaded.
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        decisionHandler(.allow)
+        
+        guard let urlAsString = navigationAction.request.url?.absoluteString.lowercased() else {
+            return
+        }
+        
+        print(urlAsString)
+
+//        if urlAsString.range(of: "message=success") != nil
+//        {
+//            self.paymentSuccess = true
+//        }
+//        else if urlAsString.range(of: "/paymentresponse.html?result=true") != nil
+//        {
+//            if self.paymentSuccess == true
+//            {
+//                self.delegate?.getUpdatedTransactions()
+//                self.closeViewController()
+//            }
+//            else
+//            {
+//                self.showError(title: "Payment Failed", message: "")
+//            }
+//        }
+
+    }
+    
+    @IBAction func crossPressed(_ sender: Any) {
+        closeView()
+    }
+    
+    func loadURL(url: String) {
+
+        let urlRequest: URL = URL(string: url)!
+
+        ktWebView.load(URLRequest(url: urlRequest))
+    }
+    
+    func closeView() {
+        dismiss()
+    }
 }
---
+++ KarwaRide/KarwaRide/KarwaRide/KTNewAddCreditCardVM.swift
@@ -0,0 +1,48 @@
+//
+//  KTNewAddCreditCardVM.swift
+//  KarwaRide
+//
+//  Created by Sam Ash on 13/12/2021.
+//  Copyright © 2021 Karwa. All rights reserved.
+//
+
+import Foundation
+
+protocol KTNewAddCreditCardVMDelegate : KTViewModelDelegate{
+    func loadURL(url: String)
+    func closeView()
+}
+
+class KTNewAddCreditCardVM: KTBaseViewModel {
+
+    var del : KTNewAddCreditCardVMDelegate?
+
+    var sessionId = ""
+    var path = ""
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        del = self.delegate as? KTNewAddCreditCardVMDelegate
+    }
+    
+    func createSession()
+    {
+        self.del?.showProgressHud(show: true, status: "str_loading".localized())
+        KTPaymentManager().createSessionForPaymentAtServer { (status, response) in
+
+            self.del?.dismiss()
+
+            if status == Constants.APIResponseStatus.SUCCESS
+            {
+                self.sessionId = (response[Constants.PaymentResponseAPIKey.SessionId] as? String)!
+                self.path = (response[Constants.PaymentResponseAPIKey.Path] as? String)!
+                self.del?.loadURL(url: self.path)
+            }
+            else
+            {
+                (self.delegate as! KTNewAddCreditCardVMDelegate).showError!(title: "error_sr".localized(),message: "please_dialog_msg_went_wrong".localized())
+            }
+        }
+    }
+}
