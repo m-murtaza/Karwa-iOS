34bc0cd4e8e93db8321dfc3acb950c7bb125b5dc
satheesh kumar <satheesh.k@molekule.com>
2022-02-16 16:40:29 +0530

    Geometry path added

--- KarwaRide/KarwaRide/KarwaRide/KTKarwaBusPlanDirectionListViewController.swift
+++ KarwaRide/KarwaRide/KarwaRide/KTKarwaBusPlanDirectionListViewController.swift
@@ -40,6 +40,20 @@ class KTKarwaBusPlanDirectionListViewController: KTBaseViewController, UITableVi
         tblView.contentInset = UIEdgeInsets(top: kTableHeaderHeight, left: 0, bottom: 0, right: 0)
         tblView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
         self.addMap()
+        
+        var i = 0
+        for item in itenary?.legs ?? [Leg]() {
+            
+            let showDestination = i == (itenary?.legs?.count ?? 0) - 1 ? true : false
+            if let routeColor = item.routeColor{
+                self.addPointsOnMap(points: item.legGeometry?.points ?? "", mode: item.mode ?? "WALK", color: UIColor(hexString: "#"+routeColor), showDestination: showDestination)
+            } else {
+                self.addPointsOnMap(points: item.legGeometry?.points ?? "", mode: item.mode ?? "WALK", color: UIColor.primary, showDestination: showDestination)
+            }
+            
+            i += 1
+        }
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -47,6 +61,83 @@ class KTKarwaBusPlanDirectionListViewController: KTBaseViewController, UITableVi
             hasSetPointOrigin = true
             pointOrigin = self.view.frame.origin
         }
+    }
+    
+    public func addPointsOnMap(points: String, mode: String, color: UIColor, showDestination: Bool) {
+      if(!points.isEmpty) {
+        // create path for router
+          if let path = GMSMutablePath(fromEncodedPath: points) {
+              let polyline = GMSPolyline.init(path: path)
+              polyline.strokeWidth = 3
+              polyline.strokeColor = color
+              
+              
+              // draw pickup and destimation pins from path
+              let pickup = path.coordinate(at:0)
+              let dropoff = path.coordinate(at:path.count()-1)
+              
+              if mode == "Walk"{
+                  let styles = [GMSStrokeStyle.solidColor(color), GMSStrokeStyle.solidColor(UIColor.clear), GMSStrokeStyle.solidColor(UIColor.clear), GMSStrokeStyle.solidColor(color)]
+                  let lengths = [0.5, 0.5] // Play with this for dotted line
+                  polyline.spans = GMSStyleSpans(polyline.path!, styles, lengths as [NSNumber], .rhumb)
+                  addMarkerOnMap(location: pickup, image: UIImage(named: "BookingMapDirectionPickup")!)
+              } else {
+                  addMarkerOnMap(location: pickup, image: UIImage(named: "stop")!)
+              }
+              
+              polyline.map = self.mapView
+              
+              if showDestination {
+                  addMarkerOnMap(location: dropoff, image: UIImage(named: "stop")!)
+              } else {
+                  addMarkerOnMap(location: dropoff, image: UIImage(named: "dropoff_pin")!)
+              }
+              
+              let inset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+              
+      //        // focus to fit all the point including path, pick and destination in map camera
+              focusMapToFitRoute(pointA: path.coordinate(at: 0),
+                                 pointB: path.coordinate(at: path.count()-1),
+                                 path: path,
+                                 inset: inset)
+                
+          }
+          
+      }
+    }
+    
+    func addMarkerOnMap(location: CLLocationCoordinate2D, image: UIImage) {
+        let marker = GMSMarker()
+        marker.position = location
+        marker.icon = image
+        marker.groundAnchor = CGPoint(x:0.5,y:1)
+        marker.map = self.mapView
+    }
+    
+    func focusMapToFitRoute(pointA: CLLocationCoordinate2D, pointB: CLLocationCoordinate2D, path: GMSMutablePath, inset: UIEdgeInsets) {
+
+      if pointA.latitude == 0 && pointA.longitude == 0 {
+        return
+      }
+
+      //var bounds: GMSCoordinateBounds
+
+      let c1 = pointA // swiftlint:disable:this identifier_name
+      let c2 = pointB // swiftlint:disable:this identifier_name
+
+      let mapCenter = CLLocationCoordinate2DMake((c1.latitude + c2.latitude)/2, (c1.longitude + c2.longitude)/2)
+
+      var bounds = GMSCoordinateBounds.init(coordinate: mapCenter, coordinate: mapCenter)
+
+      bounds = bounds.includingCoordinate(c1)
+      bounds = bounds.includingCoordinate(c2)
+      bounds = bounds.includingPath(path)
+
+      if let mutableCamera: GMSMutableCameraPosition = self.mapView.camera.mutableCopy() as? GMSMutableCameraPosition {
+        mutableCamera.target = mapCenter
+        self.mapView.camera = mutableCamera
+        self.mapView.animate(with: GMSCameraUpdate.fit(bounds, with: inset))
+      }
     }
     
     func setupViews() {
@@ -270,7 +361,8 @@ extension KTKarwaBusPlanDirectionListViewController {
                     
                     let cell : KTKarwaBusDirectionTableViewCell = tableView.dequeueReusableCell(withIdentifier: "KTKarwaBusDirectionTableViewCell") as! KTKarwaBusDirectionTableViewCell
                     cell.selectionStyle = .none
-                    cell.busNumberLbl.addLeading(image: UIImage(named: "BusListing")!, text: leg.routeShortName ?? "", imageOffsetY: -2)
+                    cell.busNumberLbl.addLeading(image: UIImage(named: "BusListing")!, text: "\(leg.routeShortName ?? "") ", imageOffsetY: 0)
+                    cell.busNumberLbl.customBorderColor = UIColor(hexString: "\(leg.routeColor ?? "")")
                     cell.addressLbl.text = leg.from?.name ?? ""
                     let date = Date(timeIntervalSince1970: TimeInterval(Double(suggestedRoutes.plan?.date ?? 0)))
                     let dateFormatter = DateFormatter()
@@ -342,7 +434,7 @@ class KTKarwaBusDirectionTableViewCell: UITableViewCell {
     @IBOutlet weak var directionStackView: UIStackView!
     @IBOutlet weak var bottomStackView: UIStackView!
     
-    @IBOutlet weak var busNumberLbl: UILabel!
+    @IBOutlet weak var busNumberLbl: PaddingLabel!
     @IBOutlet weak var addressLbl: UILabel!
     @IBOutlet weak var timeLbl: UILabel!
     @IBOutlet weak var rideToAddressLbl: UILabel!
@@ -357,6 +449,7 @@ class KTKarwaBusDirectionTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        busNumberLbl.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         // Initialization code
     }
     
